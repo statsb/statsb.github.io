a0353c3b95fbb4f526d1f2c87ec83801fa4fd425
Udbhav <uaashu@gmail.com>
2023-01-28 11:17:01 +0530

    await

---
+++ _next/static/Bz3dtRJaUIDyFiXwpvdkV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7c682bee97d9f0c6.css","static/chunks/pages/index-0b6860cc76390fa3.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
